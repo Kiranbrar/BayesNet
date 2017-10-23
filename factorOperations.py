0cac39161b80a156cf96708127073006c13887ac
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2017-10-22 19:06:26 -0700

    finished q6

--- factorOperations.py
+++ factorOperations.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -24,13 +24,13 @@ def joinFactorsByVariableWithCallTracking(callTrackingList=None):
         Input factors is a list of factors.
         Input joinVariable is the variable to join on.
 
-        This function performs a check that the variable that is being joined on 
+        This function performs a check that the variable that is being joined on
         appears as an unconditioned variable in only one of the input factors.
 
-        Then, it calls your joinFactors on all of the factors in factors that 
+        Then, it calls your joinFactors on all of the factors in factors that
         contain that variable.
 
-        Returns a tuple of 
+        Returns a tuple of
         (factors not joined, resulting factor from joinFactors)
         """
 
@@ -44,10 +44,10 @@ def joinFactorsByVariableWithCallTracking(callTrackingList=None):
         numVariableOnLeft = len([factor for factor in currentFactorsToJoin if joinVariable in factor.unconditionedVariables()])
         if numVariableOnLeft > 1:
             print "Factor failed joinFactorsByVariable typecheck: ", factor
-            raise ValueError, ("The joinBy variable can only appear in one factor as an \nunconditioned variable. \n" +  
+            raise ValueError, ("The joinBy variable can only appear in one factor as an \nunconditioned variable. \n" +
                                "joinVariable: " + str(joinVariable) + "\n" +
                                ", ".join(map(str, [factor.unconditionedVariables() for factor in currentFactorsToJoin])))
-        
+
         joinedFactor = joinFactors(currentFactorsToJoin)
         return currentFactorsNotToJoin, joinedFactor
 
@@ -58,24 +58,24 @@ joinFactorsByVariable = joinFactorsByVariableWithCallTracking()
 
 def joinFactors(factors):
     """
-    Question 3: Your join implementation 
+    Question 3: Your join implementation
 
-    Input factors is a list of factors.  
-    
-    You should calculate the set of unconditioned variables and conditioned 
+    Input factors is a list of factors.
+
+    You should calculate the set of unconditioned variables and conditioned
     variables for the join of those factors.
 
-    Return a new factor that has those variables and whose probability entries 
+    Return a new factor that has those variables and whose probability entries
     are product of the corresponding rows of the input factors.
 
-    You may assume that the variableDomainsDict for all the input 
+    You may assume that the variableDomainsDict for all the input
     factors are the same, since they come from the same BayesNet.
 
-    joinFactors will only allow unconditionedVariables to appear in 
+    joinFactors will only allow unconditionedVariables to appear in
     one input factor (so their join is well defined).
 
-    Hint: Factor methods that take an assignmentDict as input 
-    (such as getProbability and setProbability) can handle 
+    Hint: Factor methods that take an assignmentDict as input
+    (such as getProbability and setProbability) can handle
     assignmentDicts that assign more variables than are in that factor.
 
     Useful functions:
@@ -94,16 +94,16 @@ def joinFactors(factors):
         if len(intersect) > 0:
             print "Factor failed joinFactors typecheck: ", factor
             raise ValueError, ("unconditionedVariables can only appear in one factor. \n"
-                    + "unconditionedVariables: " + str(intersect) + 
-                    "\nappear in more than one input factor.\n" + 
+                    + "unconditionedVariables: " + str(intersect) +
+                    "\nappear in more than one input factor.\n" +
                     "Input factors: \n" +
                     "\n".join(map(str, factors)))
 
     "*** YOUR CODE HERE ***"
     conditionedList = list()
-    unconditionedList = list() 
+    unconditionedList = list()
     for factor in factors:
-        
+
         tempUnconditioned = factor.unconditionedVariables()
         for var in tempUnconditioned:
             if var not in unconditionedList:
@@ -115,13 +115,13 @@ def joinFactors(factors):
     conditionedList = [var for var in conditionedList if var not in unconditionedList]
 
     _newFactor = Factor(unconditionedList, conditionedList, Factor.variableDomainsDict(factors[0]))
-     
-    for assignment in _newFactor.getAllPossibleAssignmentDicts():  
-        for factor in factors:    
+
+    for assignment in _newFactor.getAllPossibleAssignmentDicts():
+        for factor in factors:
             product = 1
-            for x in factors: 
-                product *= x.getProbability(assignment)  
-        _newFactor.setProbability(assignment,product) 
+            for x in factors:
+                product *= x.getProbability(assignment)
+        _newFactor.setProbability(assignment,product)
 
     return _newFactor
 
@@ -129,13 +129,13 @@ def eliminateWithCallTracking(callTrackingList=None):
 
     def eliminate(factor, eliminationVariable):
         """
-        Question 4: Your eliminate implementation 
+        Question 4: Your eliminate implementation
 
         Input factor is a single factor.
         Input eliminationVariable is the variable to eliminate from factor.
         eliminationVariable must be an unconditioned variable in factor.
-        
-        You should calculate the set of unconditioned variables and conditioned 
+
+        You should calculate the set of unconditioned variables and conditioned
         variables for the factor obtained by eliminating the variable
         eliminationVariable.
 
@@ -159,10 +159,10 @@ def eliminateWithCallTracking(callTrackingList=None):
         if eliminationVariable not in factor.unconditionedVariables():
             print "Factor failed eliminate typecheck: ", factor
             raise ValueError, ("Elimination variable is not an unconditioned variable " \
-                            + "in this factor\n" + 
+                            + "in this factor\n" +
                             "eliminationVariable: " + str(eliminationVariable) + \
                             "\nunconditionedVariables:" + str(factor.unconditionedVariables()))
-        
+
         if len(factor.unconditionedVariables()) == 1:
             print "Factor failed eliminate typecheck: ", factor
             raise ValueError, ("Factor has only one unconditioned variable, so you " \
@@ -173,7 +173,7 @@ def eliminateWithCallTracking(callTrackingList=None):
         "*** YOUR CODE HERE ***"
         conditioned = list()
         unconditioned = list()
- 
+
         for var in factor.conditionedVariables():
             if var != eliminationVariable:
               conditioned.append(var)
@@ -186,12 +186,12 @@ def eliminateWithCallTracking(callTrackingList=None):
 
         for assignment in _newFactor.getAllPossibleAssignmentDicts():
             _sum = 0
-            for val in factor.variableDomainsDict()[eliminationVariable]:  
+            for val in factor.variableDomainsDict()[eliminationVariable]:
                 assn = copy.deepcopy(assignment)
-                assn[eliminationVariable] = val 
-                
-                _sum += factor.getProbability(assn)    
- 
+                assn[eliminationVariable] = val
+
+                _sum += factor.getProbability(assn)
+
             _newFactor.setProbability(assignment, _sum)
 
         return _newFactor
@@ -201,18 +201,18 @@ eliminate = eliminateWithCallTracking()
 
 def normalize(factor):
     """
-    Question 5: Your normalize implementation 
+    Question 5: Your normalize implementation
 
     Input factor is a single factor.
 
-    The set of conditioned variables for the normalized factor consists 
-    of the input factor's conditioned variables as well as any of the 
-    input factor's unconditioned variables with exactly one entry in their 
-    domain.  Since there is only one entry in that variable's domain, we 
-    can either assume it was assigned as evidence to have only one variable 
+    The set of conditioned variables for the normalized factor consists
+    of the input factor's conditioned variables as well as any of the
+    input factor's unconditioned variables with exactly one entry in their
+    domain.  Since there is only one entry in that variable's domain, we
+    can either assume it was assigned as evidence to have only one variable
     in its domain, or it only had one entry in its domain to begin with.
-    This blurs the distinction between evidence assignments and variables 
-    with single value domains, but that is alright since we have to assign 
+    This blurs the distinction between evidence assignments and variables
+    with single value domains, but that is alright since we have to assign
     variables that only have one value in their domain to that single value.
 
     Return a new factor where the sum of the all the probabilities in the table is 1.
@@ -222,9 +222,9 @@ def normalize(factor):
     you should return None.
 
     This is intended to be used at the end of a probabilistic inference query.
-    Because of this, all variables that have more than one element in their 
+    Because of this, all variables that have more than one element in their
     domain are assumed to be unconditioned.
-    There are more general implementations of normalize, but we will only 
+    There are more general implementations of normalize, but we will only
     implement this version.
 
     Useful functions:
@@ -243,16 +243,26 @@ def normalize(factor):
             print "Factor failed normalize typecheck: ", factor
             raise ValueError, ("The factor to be normalized must have only one " + \
                             "assignment of the \n" + "conditional variables, " + \
-                            "so that total probability will sum to 1\n" + 
+                            "so that total probability will sum to 1\n" +
                             str(factor))
 
     "*** YOUR CODE HERE ***"
-    conditionedList = list()
-    unconditionedList = list()
-    
-    for var in factor.conditionedVariables():
-        conditionedList.append(var)
-
-
-
-
+    unconditionedVariables, conditionedVariables = factor.unconditionedVariables(), factor.conditionedVariables()
+    vdd = factor.variableDomainsDict()
+    inter = set()
+    for uc in unconditionedVariables:
+        if len(vdd[uc]) == 1:
+            inter.add(uc)
+    for v in inter:
+        unconditionedVariables.remove(v)
+        conditionedVariables.add(v)
+    newFactor = Factor(unconditionedVariables, conditionedVariables, vdd)
+    total = 0
+    for d in newFactor.getAllPossibleAssignmentDicts():
+        total += factor.getProbability(d)
+    if total == 0:
+        return None
+    for d in newFactor.getAllPossibleAssignmentDicts():
+        oldProb = factor.getProbability(d)
+        newFactor.setProbability(d, oldProb / total)
+    return newFactor
